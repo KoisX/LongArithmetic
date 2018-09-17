1ee74d83eb3158e4f6e042987bb927f79d2e8962
Igor Konobas <kois9911@gmail.com>
2018-09-17 20:59:32 +0300

    Added signed opernds support in comparison operations

--- src/main/java/com/devx/NumberGreaterComparison.java
+++ src/main/java/com/devx/NumberGreaterComparison.java
@@ -1,8 +1,36 @@
 package com.devx;
 
 public class NumberGreaterComparison implements BooleanStrategy {
-    //check if a < b --> true
+    //check if a > b --> true
     public boolean execute(Number a, Number b) {
+
+        if(a.isNegative!=b.isNegative){
+            if(a.isNegative) return false;
+            else return true;
+        }
+
+        //case when both number are negative
+        if(a.isNegative && b.isNegative){
+            int length = a.value.length;
+            if (a.value.length > b.value.length) {
+                return false;
+            } else {
+                if (a.value.length < b.value.length) {
+                    return true;
+                } else {
+                    for (int i = 0; i < length; ++i) {
+                        if (a.value[i] > b.value[i]) {
+                            return false;
+                        }
+                        if (a.value[i] < b.value[i]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+
         int length = a.value.length;
         if (a.value.length > b.value.length) {
             return true;
