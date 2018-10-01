4fcb63fc663d19cb0e065277ab57177cd9ba21f8
Igor Konobas <kois9911@gmail.com>
2018-10-01 20:47:28 +0300

    System solver implemented without testing

---
+++ src/main/java/com/devx/CongruenceResult.java
@@ -0,0 +1,12 @@
+package com.devx;
+
+public class CongruenceResult {
+    //res = val + modulus * Z
+    public Number val;
+    public Number modulus;
+
+    public CongruenceResult(Number v, Number m){
+        val = v;
+        modulus = m;
+    }
+}
